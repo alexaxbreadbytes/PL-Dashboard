aa112b12fd455562576ea24d52d8576f5ce3e999
anaucoin <aucoin@math.arizona.edu>
2023-02-03 01:51:36 -0700

    lighter custom app

--- app.py
+++ app.py
@@ -87,11 +87,7 @@ def get_headers(logtype):
     #    cheader = 'Contract'
     #    plheader = ''
     #    fmat = '%Y/%m/%d %H:%M:%S' 
-    
-    if logtype == "BreadBytes Historical Data":
-        otimeheader = 'Entry Date'
-        plheader = 'Calculated P/L'
-        fmat = '%Y-%m-%d %H:%M:%S'
+
         
     if logtype == "Kraken":
         otimeheader = 'time'
@@ -170,275 +166,86 @@ def runapp() -> None:
     st.write("Welcome to the Trading Bot Dashboard by BreadBytes! You can use this dashboard to track " +
                  "the performance of our trading bots, or upload and track your own performance data from a supported exchange.")
     st.sidebar.header("FAQ")
-
+    
     with st.sidebar.subheader("FAQ"):
         st.write(Path("FAQ_README.md").read_text())
         
+    no_errors = True
     
+    exchanges = ["ByBit", "BitGet", "Binance","Kraken","MEXC","OkX"]
+    logtype = st.selectbox("Select your Exchange", options=exchanges)
+
     uploaded_data = st.file_uploader(
         "Drag and Drop files here or click Browse files.", type=[".csv", ".xlsx"], accept_multiple_files=False
     )
-    
+
+    #hack way to get button centered 
+    c = st.columns(7)
+
     if uploaded_data is None:
-        st.info("No file uploaded. Using historical data.")
-        uploaded_data = open("CT-Trade-Log.csv", "r")
+        st.info("Please upload a file.")
     else:
         st.success("Your file was uploaded successfully!")
-    
-    uploadtype = uploaded_data.name.split(".")[1]
-    if uploadtype == "csv":
-        df = pd.read_csv(uploaded_data)
-    if uploadtype == "xlsx":
-        df = pd.read_excel(uploaded_data)
-              
-    no_errors = True
 
-    with st.container():
-        col1, col2 = st.columns(2)
-        with col1:
-            st.subheader("Customize your Dashboard")
-    with st.container():
-        col1, col2 = st.columns(2)
-        with col1:
-            exchanges = ["BreadBytes Historical Data","ByBit", "BitGet", "Binance","Kraken","MEXC","OkX"]
-            logtype = st.selectbox("Select your Exchange", options=exchanges
-                      )
-            otimeheader, cheader, plheader, fmat = get_headers(logtype)
+        uploadtype = uploaded_data.name.split(".")[1]
+        if uploadtype == "csv":
+            df = pd.read_csv(uploaded_data)
+        if uploadtype == "xlsx":
+            df = pd.read_excel(uploaded_data)
             
-        with col2:
-            bots = ["Cinnamon Toast", "French Toast", "Short Bread", "Other"]
-            bot_selections = st.selectbox(
-                "Select your Trading Bot", options=bots
-                )
-            
-            symbol_selections = []
-            if logtype != "BreadBytes Historical Data" and bot_selections == "Other":
-                try:
-                    symbols = list(df[cheader].unique())
-                    symbol_selections = st.multiselect(
-                    "Select/Deselect Asset(s)", options=symbols, default=symbols
-                )
-                except:
-                    st.error("Please select your exchange or upload a supported trade log file.")
-                    no_errors = False
-                if symbol_selections == None:
-                    st.error("Please select at least one asset.")
-                    no_errors = False
-    if logtype == "BreadBytes Historical Data": 
-        if bot_selections == "Cinnamon Toast":
-            uploaded_data = open("CT-Trade-Log.csv", "r")
-            df = pd.read_csv(uploaded_data, sep='\t')
-            
-            df.columns = ['Trade','Entry Date','Buy Price', 'Sell Price','Exit Date', 'P/L per token', 'P/L %', 'Drawdown %']
-            df['Signal'] = ['Long']*len(df)
-        elif bot_selections == "Short Bread":
-            uploaded_data = open("SB-Trade-Log.csv", "r")
-            df = pd.read_csv(uploaded_data, sep='\t')
-            
-            df.columns = ['Trade','Signal','Entry Date','Buy Price', 'Sell Price','Exit Date', 'P/L per token', 'P/L %']
-        elif bot_selections == "French Toast":
-            uploaded_data = open("FT-Trade-Log.csv", "r")
-            df = pd.read_csv(uploaded_data, sep='\t')
-            
-            df.columns = ['Trade','Signal','Entry Date','Buy Price', 'Sell Price','Exit Date', 'P/L per token', 'P/L %']
-            df.dropna(inplace=True)
-        else:
-            st.error("Please select a Trading Bot.")
-            no_errors = False
+        otimeheader, cheader, plheader, fmat = get_headers(logtype)
 
-        if bot_selections == "Cinnamon Toast":
+        if not(uploaded_data is None):
             with st.container():
-                col1, col2, col3, col4 = st.columns(4)
-                with col1: 
-                    dca1 = st.number_input('DCA 1 Allocation', min_value=0, value=25, max_value= 100, step=1)
-                with col2: 
-                    dca2 = st.number_input('DCA 2 Allocation', min_value=0, value=25, max_value= 100, step=1)
-                with col3: 
-                    dca3 = st.number_input('DCA 3 Allocation', min_value=0, value=25, max_value= 100, step=1)
-                with col4: 
-                    dca4 = st.number_input('DCA 4 Allocation', min_value=0, value=25, max_value= 100, step=1)
-        else: 
-            dca1=100; dca2=0; dca3=0; dca4=0;
-    if no_errors:
-        if logtype == 'Binance':
-            otimeheader = df.filter(regex=otimeheader).columns.values[0]
-            fmat = '%Y-%m-%d %H:%M:%S'
-            df = df[df[plheader] != 0]
-        #if logtype == "Kucoin":
-        #        df = df.replace('\r\n','', regex=True) 
-        with st.container():
-            col1, col2 = st.columns(2)
-            with col1:
-                try:
-                    startdate = st.date_input("Start Date", value=pd.to_datetime(df[otimeheader]).min())
-                except:
-                    st.error("Please select your exchange or upload a supported trade log file.")
-                    no_errors = False 
-            with col2:
-                try:
-                    enddate = st.date_input("End Date", value=pd.to_datetime(df[otimeheader]).max())
-                except:
-                    st.error("Please select your exchange or upload a supported trade log file.")
-                    no_errors = False 
-            #st.sidebar.subheader("Customize your Dashboard")
+                bot_selections = "Other"
+                if bot_selections == "Other":
+                    try:
+                        symbols = list(df[cheader].unique())
+                        symbol_selections = st.multiselect(
+                        "Select/Deselect Asset(s)", options=symbols, default=symbols
+                    )
+                    except:
+                        st.error("Please select your exchange or upload a supported trade log file.")
+                        no_errors = False
+                    if symbol_selections == None:
+                        st.error("Please select at least one asset.")
+                        no_errors = False
 
-            if no_errors and (enddate < startdate): 
-                st.error("End Date must be later than Start date. Please try again.")
-                no_errors = False 
-        with st.container(): 
-            col1,col2 = st.columns(2) 
-            with col1:
-                principal_balance = st.number_input('Starting Balance', min_value=0.00, value=1000.00, max_value= 1000000.00, step=10.00)
-    
-    with st.expander("Raw Trade Log"):
-        st.write(df)
-    
-    if logtype == "BreadBytes Historical Data" and no_errors:
-        #calculate historical data cumulative performance 
-        df['Buy Price'] = df['Buy Price'].str.replace('$', '', regex=True)
-        df['Sell Price'] = df['Sell Price'].str.replace('$', '', regex=True)
-        df['Buy Price'] = df['Buy Price'].str.replace(',', '', regex=True)
-        df['Sell Price'] = df['Sell Price'].str.replace(',', '', regex=True)
-
-        df['Buy Price'] = pd.to_numeric(df['Buy Price'])
-        df['Sell Price'] = pd.to_numeric(df['Sell Price'])
-        
-        dateheader = 'Date'
-        theader = 'Time'
-
-        df[dateheader] = [tradetimes.split(" ")[0] for tradetimes in df[otimeheader].values]
-        df[theader] = [tradetimes.split(" ")[1] for tradetimes in df[otimeheader].values]
-
-        df[otimeheader]= [dateutil.parser.parse(date+' '+time)
-                                  for date,time in zip(df[dateheader],df[theader])]
-
-        #df[dateheader] = pd.to_datetime(df[dateheader])
-
-        df[otimeheader] = pd.to_datetime(df[otimeheader])
-        df['Exit Date'] = pd.to_datetime(df['Exit Date'])
-        df.sort_values(by=otimeheader, inplace=True)
-
-        df[dateheader] = [dateutil.parser.parse(date).date() for date in df[dateheader]]
-        df[theader] = [dateutil.parser.parse(time).time() for time in df[theader]]
-
-        df = df[(df[dateheader] >= startdate) & (df[dateheader] <= enddate)]
-        
-        if len(df) == 0:
-                st.error("There are no available trades matching your selections. Please try again!")
-                no_errors = False
         if no_errors:
+            if logtype == 'Binance':
+                otimeheader = df.filter(regex=otimeheader).columns.values[0]
+                fmat = '%Y-%m-%d %H:%M:%S'
+                df = df[df[plheader] != 0]
+            #if logtype == "Kucoin":
+            #        df = df.replace('\r\n','', regex=True) 
+            with st.container():
+                col1, col2 = st.columns(2)
+                with col1:
+                    try:
+                        startdate = st.date_input("Start Date", value=pd.to_datetime(df[otimeheader]).min())
+                    except:
+                        st.error("Please select your exchange or upload a supported trade log file.")
+                        no_errors = False 
+                with col2:
+                    try:
+                        enddate = st.date_input("End Date", value=pd.to_datetime(df[otimeheader]).max())
+                    except:
+                        st.error("Please select your exchange or upload a supported trade log file.")
+                        no_errors = False 
+                #st.sidebar.subheader("Customize your Dashboard")
 
-            df['DCA'] = np.nan
+                if no_errors and (enddate < startdate): 
+                    st.error("End Date must be later than Start date. Please try again.")
+                    no_errors = False 
+            with st.container(): 
+                col1,col2 = st.columns(2) 
+                with col1:
+                    principal_balance = st.number_input('Starting Balance', min_value=0.00, value=1000.00, max_value= 1000000.00, step=10.00)
 
-            for exit in pd.unique(df['Exit Date']):
-                df_exit = df[df['Exit Date']==exit]
-                for i in range(len(df_exit)):
-                    ind = df_exit.index[i]
-                    df.loc[ind,'DCA'] = i+1
+        with st.expander("Raw Trade Log"):
+            st.write(df)
 
-            dca_map = {1: dca1/100, 2: dca2/100, 3: dca3/100, 4: dca4/100}
 
-            df['DCA %'] = df['DCA'].map(dca_map)
-
-            signal_map = {'Long': 1, 'Short':-1} # 1 for long #-1 for short
-
-            df['Calculated Return %'] = (df['Signal'].map(signal_map)*((df['Sell Price']-df['Buy Price'])/df['Buy Price'])*(df['DCA %']))
-
-            df['Return Per Trade'] = np.nan
-
-            g = df.groupby('Exit Date').sum(numeric_only=True)['Calculated Return %'].reset_index(name='Return Per Trade')
-
-            df.loc[df['DCA']==1.0,'Return Per Trade'] = 1+g['Return Per Trade'].values
-
-            df['Compounded Return'] = df['Return Per Trade'].cumprod()
-            cum_pl = df.loc[df.dropna().index[-1],'Compounded Return']*principal_balance
-
-            effective_return = df.loc[df.index[-1],'Compounded Return'] - 1
-
-            st.subheader(f"Results for {bot_selections}")
-            if len(bot_selections) > 1:
-                st.metric(
-                    "Total Account Balance",
-                    f"${cum_pl:.2f}",
-                    f"{100*(cum_pl-principal_balance)/principal_balance:.2f} %",
-                )
-
-            st.subheader(f"Historical Performance")
-            df['Cumulative P/L'] = (df['Compounded Return']-1)*principal_balance
-            st.line_chart(data=df.dropna(), x='Exit Date', y='Cumulative P/L', use_container_width=True)
-
-            df['Per Trade Return Rate'] = df['Return Per Trade']-1
-
-            totals = pd.DataFrame([], columns = ['# of Trades', 'Wins', 'Losses', 'Win Rate', 'Profit Factor'])
-            data = get_hist_info(df.dropna(), principal_balance,'Per Trade Return Rate')
-            totals.loc[len(totals)] = list(i for i in data)
-
-            totals['Cum. P/L'] = cum_pl-principal_balance
-            totals['Cum. P/L (%)'] = 100*(cum_pl-principal_balance)/principal_balance
-            #results_df['Avg. P/L'] = (cum_pl-principal_balance)/results_df['# of Trades'].values[0]
-            #results_df['Avg. P/L (%)'] = 100*results_df['Avg. P/L'].values[0]/principal_balance
-
-            st.header(f"{bot_selections} Results")
-            if df.empty:
-                st.error("Oops! None of the data provided matches your selection(s). Please try again.")
-            else:
-                #st.dataframe(totals.style.format({'# of Trades': '{:.0f}','Wins': '{:.0f}','Losses': '{:.0f}','Win Rate': '{:.2f}%','Profit Factor' : '{:.2f}', 'Avg. P/L (%)': '{:.2f}%', 'Cum. P/L (%)': '{:.2f}%', 'Cum. P/L': '{:.2f}', 'Avg. P/L': '{:.2f}'})
-            #.text_gradient(subset=['Win Rate'],cmap="RdYlGn", vmin = 0, vmax = 100)\
-            #.text_gradient(subset=['Profit Factor'],cmap="RdYlGn", vmin = 0, vmax = 2), use_container_width=True)
-                for row in totals.itertuples():
-                    col1, col2, col3, col4 = st.columns(4)
-                    with col1:
-                        st.metric(
-                            "Total Trades",
-                            f"{row._1:.0f}",
-                        )
-                        st.metric(
-                            "Profit Factor",
-                            f"{row._5:.2f}",
-                        )
-                    with col2: 
-                        st.metric(
-                            "Wins",
-                            f"{row.Wins:.0f}",
-                        )
-                        st.metric(
-                            "Cumulative P/L",
-                            f"${row._6:.2f}",
-                            f"{row._7:.2f} %",
-                        )
-                    with col3: 
-                        st.metric(
-                            "Losses",
-                            f"{row.Losses:.0f}",
-                        )
-                        st.metric(
-                        "Rolling 7 Days",
-                            "",#f"{(1+get_rolling_stats(df,otimeheader, 30))*principal_balance:.2f}",
-                            f"{100*get_rolling_stats(df,otimeheader, 7):.2f}%",
-                        )
-                        st.metric(
-                        "Rolling 90 Days",
-                            "",#f"{(1+get_rolling_stats(df,otimeheader, 30))*principal_balance:.2f}",
-                            f"{100*get_rolling_stats(df,otimeheader, 90):.2f}%",
-                        )
-
-                    with col4: 
-                        st.metric(
-                            "Win Rate",
-                            f"{row._4:.1f}%",
-                        )
-                        st.metric(
-                        "Rolling 30 Days",
-                            "",#f"{(1+get_rolling_stats(df,otimeheader, 30))*principal_balance:.2f}",
-                            f"{100*get_rolling_stats(df,otimeheader, 30):.2f}%",
-                        )
-                        st.metric(
-                        "Rolling 180 Days",
-                            "",#f"{(1+get_rolling_stats(df,otimeheader, 30))*principal_balance:.2f}",
-                            f"{100*get_rolling_stats(df,otimeheader, 180):.2f}%",
-                        )
-                
-    if logtype != "BreadBytes Historical Data":
         if no_errors:
             ##cheader = df.columns[df.isin(symbol_selections[0]).any()]    
             if bot_selections == "Cinnamon Toast" or bot_selections == "Short Bread":
@@ -469,8 +276,8 @@ def runapp() -> None:
                 if len(df) == 0:
                     st.error("File Type Error. Please upload a Ledger history file from Kraken.")
                     no_errors = False
-        if no_errors:
 
+        if no_errors:
             dateheader = 'Trade Date'
             theader = 'Trade Time'
 
@@ -488,8 +295,6 @@ def runapp() -> None:
 
             df[otimeheader] = pd.to_datetime(df[otimeheader])
 
-            if logtype == "BreadBytes Historical Data":
-                df['Exit Date'] = pd.to_datetime(df['Exit Date'])
             df.sort_values(by=otimeheader, inplace=True)
             df.index = range(0,len(df))
 
