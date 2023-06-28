90a2f6691cc70b76f38552923d852312539d618d
Alexa Aucoin <123025346+alexaxbreadbytes@users.noreply.github.com>
2023-06-28 10:39:01 -0700

    Update app.py hover

--- app.py
+++ app.py
@@ -796,7 +796,7 @@ def runapp() -> None:
                     fig.add_trace(
                         go.Scatter(x=dfdata['Exit Date'], y=dfdata['Cumulative P/L'], line_shape='spline', 
                                    line = {'smoothing': 1.0, 'color' : 'rgba(31, 119, 200,.8)'},
-                                   hovertext = {'Cumulative P/L':':.2f'}, name='Cumulative P/L')
+                                   hovertemplate='(:%{dfdata["Exit Date"].values:.3f}, %{dfdata["Cumulative P/L"].values:.3f})')
                     )
                     buyhold  = (principal_balance/dfdata['Buy Price'][dfdata.index[0]])*(dfdata['Buy Price']-dfdata['Buy Price'][dfdata.index[0]])
                     fig.add_trace(go.Scatter(x=dfdata['Exit Date'], y=np.round(buyhold.values,2), line_shape='spline', line = {'smoothing': 1.0, 'color' :'red'}, name = 'Buy & Hold Return')
